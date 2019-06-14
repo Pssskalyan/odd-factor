76cf9c2c57df123db570bea23af984a7d1444bee
PAVANKALYAN <41368953+Pssskalyan@users.noreply.github.com>
2019-06-14 16:23:16 +0530

    Update pavan.py

--- pavan.py
+++ pavan.py
@@ -1,6 +1,8 @@
 x = int(input())
-st = ''
+st = []
 for i in range(1, x + 1):
     if x % i == 0:
-    	st += (str(i)+" ")
-print(st)
+        st.append(i)
+            k=' '.join(map(str,st))  
+print(k)
+
